87efd95c6c27070e331abb0f2ecdb22ec8ad884e
Dev Shah <65584840+CodeRustyPro@users.noreply.github.com>
2020-10-24 15:38:55 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+int main(){
+	float a,b,result;
+	printf("Enter a number :");
+	scanf("%f",&a);
+	printf("Enter the second Number : ");
+	scanf("%f",&b);
+	result = a*b;
+	printf("%f",result);
+	return 0;
+}
