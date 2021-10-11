8740ec84e094397be534d1c9e6997f0a8ebfd892
Haardispro <haardmajmudar2827@gmail.com>
2021-10-11 09:07:18 +0530

    added comments

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 
 #include <stdio.h>
 int main(){
+	//declaring the variables
 	float a,b,result;
+	//input
 	printf("Enter a number :");
 	scanf("%f",&a);
 	printf("Enter the second Number : ");
 	scanf("%f",&b);
+	//result 
 	result = a*b;
 	printf("%f",result);
 	return 0;
